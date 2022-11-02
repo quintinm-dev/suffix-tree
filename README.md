8597a807a35d66fcc567f60528d37e2121ff622c
Quintin Muncaster <12143900+quintinm-dev@users.noreply.github.com>
2022-11-01 20:51:02 -0700

    Implement naive O(n^3) algorithm

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-## Linting
+## Lint
 ```bash
-black -l *.py
-pylint --rcfile=pylintrc *.py
+black -l 80 **/*.py
+pylint --rcfile=pylintrc **/*.py
+```
+
+## Tests
+```bash
+python3 -m unittest tests.suffix_tree_test
 ```
---
+++ tests/suffix_tree_test.py
@@ -0,0 +1,80 @@
+"""Unit tests for SuffixTree."""
+
+import unittest
+import random
+import string
+from src import suffix_tree
+
+SuffixTree = suffix_tree.SuffixTree
+
+
+class SuffixTreeTest(unittest.TestCase):
+    longMessage = True
+
+    def _setup_tree(self, word: str) -> SuffixTree:
+        tree = SuffixTree(word)
+        tree.build()
+        return tree
+
+    def _assert_all_suffixes_present(self, tree: SuffixTree, word: str):
+        for i in range(len(word)):
+            suffix = word[i:]
+            self.assertTrue(
+                tree.find(suffix), f"word: {word}, suffix: {suffix}"
+            )
+
+    def _random_word(self, length: int):
+        return "".join(
+            random.choice(string.ascii_lowercase) for _ in range(length)
+        )
+
+    def test_uncompressable(self):
+        word = "abcde"
+        tree = self._setup_tree(word)
+
+        self._assert_all_suffixes_present(tree, word)
+
+        self.assertFalse(tree.find("bcdef"))
+        self.assertFalse(tree.find("ce"))
+        self.assertFalse(tree.find("ax"))
+
+    def test_rule_3(self):
+        # https://stackoverflow.com/a/9513423 has diagram for comparison
+        word = "abcabxabcd"
+        tree = self._setup_tree(word)
+
+        self._assert_all_suffixes_present(tree, word)
+
+        self.assertFalse(tree.find("k"))
+        self.assertFalse(tree.find("yd"))
+        self.assertFalse(tree.find("bcx"))
+        self.assertFalse(tree.find("cabxabd"))
+
+    def test_rule_3_harder(self):
+        words = [
+            "savannas",
+            "ogopogo",
+            "oniononiono",
+            "aaaaaaaaaaaaa",
+            "hahaaahaahaaaa",
+            "abcdefabxybcdmnabcdex",
+            "aaaabaaaabaac",
+            "aabaaabb",
+        ]
+        for word in words:
+            tree = self._setup_tree(word)
+            self._assert_all_suffixes_present(tree, word)
+
+    def test_dynamic_short(self):
+        word_length = 8
+        iterations = 50
+
+        for _ in range(iterations):
+            word = self._random_word(word_length)
+            tree = self._setup_tree(word)
+            self._assert_all_suffixes_present(tree, word)
+            # TODO: test more queries that should return False
+
+
+if __name__ == "__main__":
+    unittest.main()
